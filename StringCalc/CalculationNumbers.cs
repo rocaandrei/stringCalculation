2f92d2dcd794c71423a2d888130b628c1ea6ead7
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-09-26 21:58:32 +0300

    Add project files.

---
+++ StringCalc/CalculationNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalc
+{
+    public class CalculationNumbers//in test inca...
+    {
+        int _number1;
+        int _number2;
+        List<char> _operationSymbol;
+        string[] digits;
+
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public char OperationSymbol { get; set; }
+
+        public CalculationNumbers( string calculationWanted)
+        {
+            _operationSymbol = new List<char>();
+            if (calculationWanted.Contains("+"))
+            {
+                digits = calculationWanted.Split('+');
+                _operationSymbol.Add('+');
+            }
+            if (calculationWanted.Contains("-"))
+            {
+                digits = calculationWanted.Split('-');
+                _operationSymbol.Add('-');
+               
+            }
+        }
+
+    }
+}
---
+++ StringCalc/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string addStr = "22+1+5-6";
+            //TO DO: daca ii schimb ordinea operatiilor matematice de ex: 22-5+6 nu mai merge si aici ar trebui sa aplic chain of responsability... cred
+            //adica cand se schimba symbolul initial sa schimb si eu operatiunea
+            int x = Add(RemoveSpace(addStr));
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+
+        static int Add(string calculation)
+        {
+            char symbol = '+';
+            int index = calculation.IndexOf(symbol);
+            int a = 0;
+            int b = 0;
+            int result = 0;
+
+            if (index >= 0)
+            {
+                string strA = calculation.Substring(0, index);
+                string strB = calculation.Substring(index + 1);
+                if (!int.TryParse(strB, out b))
+                {
+                    if (strB.Contains('+'))
+                    {
+                        b = Add(strB);
+                    }
+                    else if (strB.Contains('-'))
+                    {
+                        b = Subtract(strB);
+                    }
+                }
+                a = int.Parse(strA);
+            }
+            result = a + b;
+            return result;
+        }
+        static string RemoveSpace(string initial)
+        {
+            return initial.Replace(" ", string.Empty);
+        }
+        static int Subtract(string calculation)
+        {
+            char symbol = '-';
+            int index = calculation.IndexOf(symbol);
+            int a = 0;
+            int b = 0;
+            int result = 0;
+            if (index >= 0)
+            {
+                string strA = calculation.Substring(0, index);
+                string strB = calculation.Substring(index + 1);
+                if (!int.TryParse(strB, out b))
+                {
+                    b = Subtract(strB);
+                }
+                a = int.Parse(strA);
+            }
+            result = a - b;
+            return result;
+        }
+    }
+}
